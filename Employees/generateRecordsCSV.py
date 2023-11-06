950da20ecc5baa290d4863fd8dd0b22b2c25cae2
Bohdan Wazer <ki4ack.bogdan@gmail.com>
2023-11-06 22:26:54 +0200

    test

--- Employees/generateRecordsCSV.py
+++ Employees/generateRecordsCSV.py
@@ -1,47 +1,64 @@
 import csv
+import sys
 from faker import Faker
+import random
 
-fake = Faker()
+fake = Faker('uk_UA')  # Використовуємо українську локаль для Faker
 
-# задаю кількість записів
+# Задаємо кількість записів
 num_records = 2000
 
-# задаю відсоток жіночої та чоловічої статі
-num_female = int(0.4 * num_records)
-num_male = int(0.6 * num_records)
-
-# задаю діапазон для року народження (від 1938 до 2008 року)
+# Задаємо діапазон для року народження (від 1938 до 2008 року)
 start_birth_year = 1938
 end_birth_year = 2008
 
-# генерація та збереження даних
-with open('employees.csv', 'w', newline='') as csvfile:
-    fieldnames = ['Прізвище', 'Ім’я', 'Стать', 'Дата народження', 'Посада', 'Місто проживання', 'Адреса проживання', 'Телефон', 'Email'] # прописання назви полів
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames) # запис даних
-    
+# Функція для генерації випадкового по-батькові
+
+
+def generate_middle_name():
+    male_middle_names = [
+        "Олексійович",
+        "Андрійович",
+        "Васильович",
+        "Іванович",
+        "Петрович",
+    ]
+
+    female_middle_names = [
+        "Олександрівна",
+        "Андріївна",
+        "Василівна",
+        "Іванівна",
+        "Петрівна",
+    ]
+
+    gender = random.choice(["Чоловіча", "Жіноча"])
+    if gender == "Чоловіча":
+        return random.choice(male_middle_names)
+    else:
+        return random.choice(female_middle_names)
+
+
+# Встановлюємо кодування 'utf-8' для стандартного виведення
+sys.stdout = open(sys.stdout.fileno(), mode='w', encoding='utf-8', buffering=1)
+
+# Генерація та збереження даних
+with open('employees.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    fieldnames = ['Прізвище', 'Ім’я', 'По-батькові', 'Стать', 'Дата народження', 'Посада',
+                  'Місто проживання', 'Адреса проживання', 'Телефон', 'Email']  # прописання назви полів
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)  # запис даних
+
     writer.writeheader()
-    
-    for _ in range(num_female): # _ заміняє індекс, щоб його не використовувати взагалі
-        gender = 'Жіноча'
-        birth_date = fake.date_of_birth(tzinfo=None, minimum_age=2023 - end_birth_year, maximum_age=2023 - start_birth_year)
+
+    for _ in range(num_records):
+        gender = random.choice(["Чоловіча", "Жіноча"])
+        birth_date = fake.date_of_birth(
+            tzinfo=None, minimum_age=2023 - end_birth_year, maximum_age=2023 - start_birth_year)
+        middle_name = generate_middle_name()
         writer.writerow({
             'Прізвище': fake.last_name(),
-            'Ім’я': fake.first_name_female(),
-            'Стать': gender,
-            'Дата народження': birth_date.strftime('%d/%m/%Y'),
-            'Посада': fake.job(),
-            'Місто проживання': fake.city(),
-            'Адреса проживання': fake.address(),
-            'Телефон': fake.phone_number(),
-            'Email': fake.email()
-        })
-    
-    for _ in range(num_male):
-        gender = 'Чоловіча'
-        birth_date = fake.date_of_birth(tzinfo=None, minimum_age=2023 - end_birth_year, maximum_age=2023 - start_birth_year)
-        writer.writerow({
-            'Прізвище': fake.last_name(),
-            'Ім’я': fake.first_name_male(),
+            'Ім’я': fake.first_name_female() if gender == 'Жіноча' else fake.first_name_male(),
+            'По-батькові': middle_name,
             'Стать': gender,
             'Дата народження': birth_date.strftime('%d/%m/%Y'),
             'Посада': fake.job(),
@@ -51,4 +68,5 @@ with open('employees.csv', 'w', newline='') as csvfile:
             'Email': fake.email()
         })
 
-print(f'Згенеровано {num_records} записів у файл "employees.csv"') # виводимо результати
+# виводимо результати
+print(f'Згенеровано {num_records} записів у файл "employees.csv"')
--- Employees/sortinxlsx.py
+++ Employees/sortinxlsx.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from datetime import datetime
 
-# Завантажимо дані з CSV файлу
+# Завантаження даних з CSV файлу
 try:
-    df = pd.read_csv('employees.csv', encoding='Windows-1251', engine='python')
+    df = pd.read_csv('employees.csv', encoding='utf-8')
 except FileNotFoundError:
     print('Помилка: файл CSV не знайдено.')
     exit(1)
@@ -11,26 +11,43 @@ except Exception as e:
     print(f'Помилка при завантаженні CSV файлу: {str(e)}')
     exit(1)
 
-# Перетворення стовпця "Дата народження" у тип datetime та обчислення віку
-df['Дата народження'] = pd.to_datetime(df['Дата народження'], format='%d/%m/%Y', errors='coerce')
-df.dropna(subset=['Дата народження'], inplace=True)  # Видалимо рядки з некоректною датою
-current_date = datetime.now()
-df['Вік'] = current_date.year - df['Дата народження'].dt.year
+# Перевірка наявності стовпця "Дата народження"
+if 'Дата народження' in df.columns:
+    # Перетворення стовпця "Дата народження" у тип datetime.date
+    df['Дата народження'] = pd.to_datetime(
+        df['Дата народження'], format='%d/%m/%Y', errors='coerce').dt.date
 
-# Створення XLSX файлу
-try:
-    with pd.ExcelWriter('employees.xlsx', engine='xlsxwriter') as writer:
-        # Аркуш "all"
-        df.to_excel(writer, sheet_name='all', index=False)
+    # Видалення рядків з некоректною датою
+    df = df.dropna(subset=['Дата народження'])
 
-        # Аркуши з віковими категоріями
-        age_bins = [0, 18, 45, 70, float('inf')]
-        age_labels = ['younger_18', '18-45', '45-70', 'older_70']
+    # Функція для обчислення віку з урахуванням місяців
+    def calculate_age(birth_date):
+        today = datetime.now()
+        age = today.year - birth_date.year - \
+            ((today.month, today.day) < (birth_date.month, birth_date.day))
+        return age
 
-        for i in range(len(age_bins) - 1):
-            age_range_df = df[(df['Вік'] > age_bins[i]) & (df['Вік'] <= age_bins[i+1])]
-            age_range_df[['Прізвище', 'Ім’я', 'Дата народження', 'Вік']].to_excel(writer, sheet_name=age_labels[i], index=False)
+    # Обчислення віку з урахуванням місяців
+    df['Вік'] = df['Дата народження'].apply(calculate_age)
 
-    print('Ok, програма завершила свою роботу успішно.')
-except Exception as e:
-    print(f'Помилка: неможливо створити XLSX файл. {str(e)}')
+    # Створення XLSX файлу
+    try:
+        with pd.ExcelWriter('employees.xlsx', engine='xlsxwriter') as writer:
+            # Аркуш "all"
+            df.to_excel(writer, sheet_name='all', index=False)
+
+            # Аркуши з віковими категоріями
+            age_bins = [0, 18, 45, 70, float('inf')]
+            age_labels = ['younger_18', '18-45', '45-70', 'older_70']
+
+            for i in range(len(age_bins) - 1):
+                age_range_df = df[(df['Вік'] > age_bins[i])
+                                  & (df['Вік'] <= age_bins[i+1])]
+                age_range_df[['Прізвище', 'Ім’я', 'Дата народження', 'Вік']].to_excel(
+                    writer, sheet_name=age_labels[i], index=False)
+
+        print('Ok, програма завершила свою роботу успішно.')
+    except Exception as e:
+        print(f'Помилка: неможливо створити XLSX файл. {str(e)}')
+else:
+    print('Помилка: стовпець "Дата народження" відсутній у файлі.')
